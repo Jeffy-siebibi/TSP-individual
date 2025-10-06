f00c84b58a207c834977853ba1216e64212a91ad
Jeffy-siebibi <jsiebig55@gmail.com>
2025-10-05 20:48:49 -0400

    added a line that will throw errors to practice reverting changes

--- fileTwo.java
+++ fileTwo.java
@@ -1,3 +1,4 @@
 public static void main(String[] args){
   system.out.println("yo I just realized i capitalized the m in main onmy first file, ima need to fix that");
+  oh man i sure hope this line doesnt cause any problems with the code
 }
