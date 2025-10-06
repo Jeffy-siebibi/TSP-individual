b0e8e338c5f590bc354053a85bb77ac0d8cccf3a
Jeffy-siebibi <jsiebig55@gmail.com>
2025-10-05 21:14:20 -0400

    Revert "added a line that will throw errors to practice reverting changes"
    This reverts commit f00c84b58a207c834977853ba1216e64212a91ad.

--- fileTwo.java
+++ fileTwo.java
@@ -1,4 +1,3 @@
 public static void main(String[] args){
   system.out.println("yo I just realized i capitalized the m in main onmy first file, ima need to fix that");
-  oh man i sure hope this line doesnt cause any problems with the code
 }
