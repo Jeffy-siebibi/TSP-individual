c747533122f15de8ec3d2d73bd3a0f87d47d1719
Jeffy-siebibi <jsiebig55@gmail.com>
2025-10-05 21:17:35 -0400

    added fileTwo class, fixed a spelling error on System

--- fileTwo.java
+++ fileTwo.java
@@ -1,3 +1,5 @@
-public static void main(String[] args){
-  system.out.println("yo I just realized i capitalized the m in main onmy first file, ima need to fix that");
+public class fileTwo{
+  public static void main(String[] args){
+    System.out.println("yo I just realized i capitalized the m in main onmy first file, ima need to fix that");
+  }
 }
